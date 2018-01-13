f9a3ff03d3c49ff4f3d23032b1c4e6016ccb1120
Bertrand B <boyer.bertrand@gmail.com>
2018-01-13 13:11:31 +0100

    Core interfaces (Client & Message).

---
+++ src/main/java/com/brtrndb/chatscript/client/core/CSMessage.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.brtrndb.chatscript.client.core;
+
+/**
+ * @author bertrand
+ * @see Full Chatscript documentation on implementation available at https://github.com/bwilcox-1234/ChatScript.
+ */
+public interface CSMessage
+{
+	/**
+	 * Transform a message into the ChatScript message format.
+	 * 
+	 * @return
+	 */
+	public byte[] toCSFormat();
+}
