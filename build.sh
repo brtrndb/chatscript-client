554f4a0e00f4abff5f39496e24db0c7ca42493fe
Bertrand B <boyer.bertrand@gmail.com>
2018-01-14 20:50:45 +0100

    Added simple script to init repo and build jar.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Get submodules.";
+git submodule init;
+git submodule update;
+
+chmod -v +x ChatScript/BINARIES/LinuxChatScript64;
+
+echo "Building jar."
+mvn clean test package;
+
+
+echo "";
+echo "To test, run './ChatScript/BINARIES/LinuxChatScript64 port=2828' in a terminal.";
+echo "Then run 'java -jar chatscript-client-0.0.1-jar-with-dependencies.jar localhost 2828'."
