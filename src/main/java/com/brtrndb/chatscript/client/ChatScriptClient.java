0f7e4e854328b207966de07f6eaf1bd7246b7013
Bertrand B <boyer.bertrand@gmail.com>
2018-01-25 19:01:49 +0100

    Updated options.

--- src/main/java/com/brtrndb/chatscript/client/ChatScriptClient.java
+++ src/main/java/com/brtrndb/chatscript/client/ChatScriptClient.java
@@ -10,16 +10,13 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import com.brtrndb.chatscript.client.core.ChatscriptException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ChatScriptClient
 {
-	private static final String	DEFAULT_USERNAME	= "user";
-	private static final String	DEFAULT_BOTNAME		= "harry";
-	private static final String	DEFAULT_SERVER_URL	= "localhost";
-	private static final String	DEFAULT_SERVER_PORT	= "1024";
-
 	public static void main(final String[] args)
 	{
 		try
@@ -37,33 +34,53 @@ public class ChatScriptClient
 				clientManager.start();
 			}
 		}
+		catch (ChatscriptException e)
+		{
+			log.error("Chatscript client error", e);
+			System.out.println("An error occurs: " + e.getLocalizedMessage() + ".");
+		}
 		catch (final Exception e)
 		{
-			log.error("Oups... Something went wrong: ", e.getLocalizedMessage());
+			log.error("Oups... Something unexpected went wrong: ", e);
+			System.out.println("An unexpected error occurs: " + e.getLocalizedMessage() + ".");
 		}
 	}
 
-	private static Entry<Options, CommandLine> verifyCli(final String[] args) throws ParseException
+	private static Entry<Options, CommandLine> verifyCli(final String[] args) throws ChatscriptException
 	{
-		final Options helpOptions = ChatScriptOptions.helpOptions();
-		final Options cliOptions = ChatScriptOptions.cliOptions();
-		final Options allOptions = ChatScriptOptions.merge(helpOptions, cliOptions);
+		try
+		{
+			final Options helpOptions = ChatScriptOptions.helpOptions();
+			final Options cliOptions = ChatScriptOptions.cliOptions();
+			final Options allOptions = ChatScriptOptions.merge(helpOptions, cliOptions);
 
-		final CommandLineParser parser = new DefaultParser();
-		final CommandLine cli = parser.parse(allOptions, args, true);
+			final CommandLineParser parser = new DefaultParser();
+			final CommandLine cli = parser.parse(allOptions, args, true);
 
-		final SimpleEntry<Options, CommandLine> entry = new SimpleEntry<>(allOptions, cli);
-		return (entry);
+			final SimpleEntry<Options, CommandLine> entry = new SimpleEntry<>(allOptions, cli);
+			return (entry);
+		}
+		catch (ParseException e)
+		{
+			throw (new ChatscriptException("Could not parse command line", e));
+		}
 	}
 
-	private static ClientManager createClientManager(final CommandLine cli)
+	private static ClientManager createClientManager(final CommandLine cli) throws ChatscriptException
 	{
-		final String url = cli.getOptionValue("u", DEFAULT_SERVER_URL);
-		final int port = Integer.parseInt(cli.getOptionValue("p", DEFAULT_SERVER_PORT));
-		final String username = cli.getOptionValue("n", DEFAULT_USERNAME);
-		final String botname = cli.getOptionValue("b", DEFAULT_BOTNAME);
+		try
+		{
+			final String url = cli.getOptionValue("u", ChatScriptOptions.DEFAULT_SERVER_URL);
+			final int port = Integer.parseInt(cli.getOptionValue("p", ChatScriptOptions.DEFAULT_SERVER_PORT));
+			final String username = cli.getOptionValue("n", ChatScriptOptions.DEFAULT_USERNAME);
+			final String botname = cli.getOptionValue("b", ChatScriptOptions.DEFAULT_BOTNAME);
 
-		final ClientManager clientManager = new ClientManager(url, port, username, botname);
-		return (clientManager);
+			final ClientManager clientManager = new ClientManager(url, port, username, botname);
+			return (clientManager);
+		}
+		catch (NumberFormatException e)
+		{
+			throw (new ChatscriptException("Port parameter in not a number", e));
+		}
 	}
 }
--- src/main/java/com/brtrndb/chatscript/client/ChatScriptOptions.java
+++ src/main/java/com/brtrndb/chatscript/client/ChatScriptOptions.java
@@ -5,13 +5,20 @@ import org.apache.commons.cli.Options;
 
 public class ChatScriptOptions
 {
+	public static final String	DEFAULT_USERNAME	= "user";
+	public static final String	DEFAULT_BOTNAME		= "harry";
+	public static final String	DEFAULT_SERVER_URL	= "localhost";
+	public static final String	DEFAULT_SERVER_PORT	= "1024";
+
 	public static Options helpOptions()
 	{
 		final Options options = new Options();
 
 		final Option optionHelp = Option.builder("h")
 				.longOpt("help")
-				.desc("Display help usage")
+				.desc("Display help usage.")
+				.hasArg(false)
+				.required(false)
 				.build();
 
 		options.addOption(optionHelp);
@@ -25,33 +32,40 @@ public class ChatScriptOptions
 
 		final Option optionUrl = Option.builder("u")
 				.longOpt("url")
-				.desc("Server url")
-				.argName("url")
+				.desc("ChatScript server url. Default is '" + DEFAULT_SERVER_URL + "'.")
 				.hasArg(true)
+				.numberOfArgs(1)
+				.argName("url")
+				.type(String.class)
 				.required(false)
 				.build();
 
 		final Option optionPort = Option.builder("p")
 				.longOpt("port")
-				.desc("Server port")
+				.desc("Chatscript server port. Default is '" + DEFAULT_SERVER_PORT + "'.")
 				.argName("port")
 				.hasArg(true)
+				.type(Integer.class)
 				.required(false)
 				.build();
 
 		final Option optionUsername = Option.builder("n")
 				.longOpt("name")
-				.desc("Username")
-				.argName("name")
+				.desc("Username. Default is '" + DEFAULT_USERNAME + "'.")
 				.hasArg(true)
+				.numberOfArgs(1)
+				.argName("name")
+				.type(String.class)
 				.required(false)
 				.build();
 
 		final Option optionBotname = Option.builder("b")
 				.longOpt("bot")
-				.desc("Botname")
-				.argName("botname")
+				.desc("Botname. Default is '" + DEFAULT_BOTNAME + "'.")
 				.hasArg(true)
+				.numberOfArgs(1)
+				.argName("botname")
+				.type(String.class)
 				.required(false)
 				.build();
 
